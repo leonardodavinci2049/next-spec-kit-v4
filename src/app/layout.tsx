fd5c8d056a6cf53e88753af3d01466df575513ef
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-10-25 18:10:55 -0300

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header02 from "@/components/header/Header02";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header02 />
+
+          <div className="container mx-auto px-4 py-12">{children}</div>
+        </ThemeProvider>
       </body>
     </html>
   );
